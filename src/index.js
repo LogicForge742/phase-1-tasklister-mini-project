5c1fd94b1fab9c7f03c7247156ae9fbd4eb34c7f
Milton Ngeno <ngenomilton10@gmail.com>
2025-07-08 17:40:19 +0300

    adds  SA Week 3 Code Challenge: TaskLister Mini-Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.getElementById("create-task-form")
+  form.addEventListener("submit" ,(e)=>{
+    e.preventDefault()
+    
+    const input = document.getElementById("new-task-description")
+    const task = input.value
+      if(task === ""){
+          alert("Please enter a task")
+        }
+
+    const showTask =document.createElement("li")
+
+        showTask.textContent = task
+  
+      
+    document.getElementById("tasks").appendChild(showTask)
+           let btn = document.createElement("button")
+           btn.addEventListener("click" ,handleBtn )
+           btn.textContent = "x"
+           showTask.appendChild(btn)
+           btn.style.marginLeft = "10px";
+
+ form.reset()
+           
+  })
+  function handleBtn(e){
+    e.target.parentNode.remove()
+  }
+  
 });
